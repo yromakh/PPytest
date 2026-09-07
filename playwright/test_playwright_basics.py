0a734ecccb4fe23cf5d5d957902c1b67e383a655
Yaroslav Romakh <yaroslav.romakh@avenga.com>
2026-09-07 12:26:01 +0300

    added tests on credentials validation and sign in

--- playwright/test_playwright_basics.py
+++ playwright/test_playwright_basics.py
@@ -1,7 +1,7 @@
 import re
 import time
 
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page, Playwright, expect
 
 def test_playwrightBasics(playwright):
     browser = playwright.chromium.launch(headless=False)
@@ -17,19 +17,30 @@ def test_playwrightShortCut(page):
 def test_open_practice_site(page: Page):
     page.goto("https://rahulshettyacademy.com/practice")
     
-def test_core_locators(page: Page):
+def test_core_locators_sign_in(page: Page):
     page.goto("https://rahulshettyacademy.com/loginpagePractise/")
     page.locator("#username").fill("rahulshettyacademy")
-    page.locator("#password").fill("Learning@830$3mK2")
-    
+    page.locator("#password").fill("Learning@830$3mK2")    
     page.get_by_role("combobox").select_option("teach")
-    page.get_by_role("link", name="terms and conditions").click()
     page.locator("#terms").check()
+    page.get_by_role("link", name="terms and conditions").click()
     page.get_by_role("button", name="Sign In").click()
     
-    expect(page.get_by_role("heading", name="Shop Name")).to_be_visible()
     # assert "Shop Name" in page.title()
-    time.sleep(5)
+    expect(page.get_by_role("heading", name="Shop Name")).to_be_visible()
+    
+def test_incorrect_credentials(page: Page):
+    page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    page.locator("#username").fill("rahulshettyacademy_wrong")
+    page.locator("#password").fill("Learning@830$3mK2_wrong")    
+    page.get_by_role("combobox").select_option("teach")
+    page.locator("#terms").check()
+    page.get_by_role("link", name="terms and conditions").click()
+    page.get_by_role("button", name="Sign In").click()
+    
+    # Checking message when credentials are incorrect: "Incorrect username/password."
+    expect(page.locator(".alert.alert-danger.col-md-12")).to_be_visible() # works
+    expect(page.get_by_text("Incorrect username/password.")).to_be_visible() # works
 
 def test_user_is_redirected_to_interview_page_successfully(page: Page):
     page.goto("https://rahulshettyacademy.com/loginpagePractise/")
@@ -39,4 +50,18 @@ def test_user_is_redirected_to_interview_page_successfully(page: Page):
     expect(page1.get_by_role("heading", name="Documents request")).to_be_visible()
     
     time.sleep(5)
+
+def test_firefox_browser(playwright: Playwright):
+    browser = playwright.firefox.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
     
+    page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    page.locator("#username").fill("rahulshettyacademy")
+    page.locator("#password").fill("Learning@830$3mK2")    
+    page.get_by_role("combobox").select_option("teach")
+    page.locator("#terms").check()
+    page.get_by_role("link", name="terms and conditions").click()
+    page.get_by_role("button", name="Sign In").click()
+    
+    expect(page.get_by_role("heading", name="Shop Name")).to_be_visible()
